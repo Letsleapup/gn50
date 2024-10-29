2ab742324cec54327d582e9e30bb5d905cc9d86c
visvaden7 <visvaden7@naver.com>
2024-10-29 10:10:13 +0900

    refactor: refactor filter by Korean,
    add press consonant and vowel

--- frontend/src/util/filterText.ts
+++ frontend/src/util/filterText.ts
@@ -1,3 +1,3 @@
 export const filterTextByKorean = (text: string) => {
-  return text.replace(/[^\uAC00-\uD7A3\u1100-\u11FF0-9\s!@#$%^&*(),.?":{}|<>~`']/g, "");
+  return text.replace(/[^\uAC00-\uD7A3\u1100-\u11FF\u3131-\u318E0-9\s!@#$%^&*(),.?":{}|<>~`']/g, "");
 };
