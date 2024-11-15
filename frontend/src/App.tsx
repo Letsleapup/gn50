ea5825393fb1ff204e84af7f34e5057065fa347c
visvaden7 <visvaden7@naver.com>
2024-11-15 13:49:41 +0900

    refactor: hashRouter -> BrouserRouter in Appjs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import ShareBoardPage from "./pages/shareboardpage/ShareBoardPage";
 import MainPage from "./pages/mainpage/MainPage";
