0887448d7917b278a439ad52b694d18123f745b1
Anastacia(박초롱) <155081933+helloghostt@users.noreply.github.com>
2024-11-19 10:23:17 +0900

    chatbotpage's header,footer hidden.  modal.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import "./index.css";
 import ShareBoardPage from "./pages/shareboardpage/ShareBoardPage";
 import MainPage from "./pages/mainpage/MainPage";
@@ -13,34 +18,45 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import { ScrollToTop } from "./util/ScrollToTop";
 
+// AppRoutes 컴포넌트 - 실제 라우팅 로직을 담당
+const AppRoutes: React.FC = () => {
+  const location = useLocation();
+  const isChatbotPage = location.pathname.includes("/chatbot");
+
+  return (
+    <div className="App">
+      {!isChatbotPage && <Header />}
+      <ScrollToTop />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="chatbot/:type" element={<ChatbotPage />} />
+          <Route path="select/:type" element={<SelectPage />} />
+          <Route path="shared/:type" element={<ShareBoardPage />} />
+          <Route
+            path="loading"
+            element={<Loading message={starterMessage} />}
+          />
+          <Route
+            path="shared/:type/:contentId"
+            element={<DetailContent source="shareboard" />}
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+      {!isChatbotPage && <Footer />}
+    </div>
+  );
+};
+
+// 메인 App 컴포넌트 - Router 설정을 담당
 const App: React.FC = () => {
   return (
     <Router
       basename=""
       future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
     >
-      <div className="App">
-        <Header />
-        <ScrollToTop />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="chatbot/:type" element={<ChatbotPage />} />
-            <Route path="select/:type" element={<SelectPage />} />
-            <Route path="shared/:type" element={<ShareBoardPage />} />
-            <Route
-              path="loading"
-              element={<Loading message={starterMessage} />}
-            />
-            <Route
-              path="shared/:type/:contentId"
-              element={<DetailContent source="shareboard" />}
-            />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppRoutes />
     </Router>
   );
 };
