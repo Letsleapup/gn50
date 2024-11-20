41da70101a510f38db0b630cc74157d6a12cf625
Anastacia(박초롱) <155081933+helloghostt@users.noreply.github.com>
2024-11-20 16:13:44 +0900

    chatbot-scroll, modal-css

--- frontend/src/pages/chatbotpage/ChatbotPage.tsx
+++ frontend/src/pages/chatbotpage/ChatbotPage.tsx
@@ -46,6 +46,18 @@ const ChatbotPage: React.FC<ChatbotPageProps> = ({ onShowResult }) => {
       scenario: "",
     });
 
+  useEffect(() => {
+    const setVh = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => window.removeEventListener("resize", setVh);
+  }, [window.innerHeight]);
+
   // 웰컴 메세지
   useEffect(() => {
     if (!type || !title || !imgUrl) {
