94d12d7fd57bf8b2188775c90367c1b0fb61ce0f
visvaden7 <visvaden7@naver.com>
2024-11-21 11:40:36 +0900

    refactor: remove cached file

--- frontend/src/util/filterText.ts
+++
@@ -1,3 +0,0 @@
-export const filterTextByKorean = (text: string) => {
-  return text.replace(/[^\uAC00-\uD7A3\u1100-\u11FF\u3131-\u318E0-9\s!@#$%^&*(),.?":{}|<>~`']/g, "");
-};
--- frontend/src/util/isArray.ts
+++
@@ -1,3 +0,0 @@
-export function isArray<T>(input: T | T[]): T[] {
-  return Array.isArray(input) ? input : [input];
-}
