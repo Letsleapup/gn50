e9d63352fb64e619b2c5efea4cd1a6d6d87e84d7
Anastacia(박초롱) <155081933+helloghostt@users.noreply.github.com>
2024-11-27 11:53:19 +0900

    connect chatbotpage-api

--- frontend/src/api/profanity_api.ts
+++ frontend/src/api/profanity_api.ts
@@ -6,19 +6,10 @@ interface ProfanityResponse {
   resultCode: "Y" | "N";
 }
 export const profanityApi = {
-  checkProfanity: async (text: string) => {
-    try {
-      logger.info(`Profanity Check Text: ${text}`);
-      const response = await axios.get<ProfanityResponse>(
-        `${BASE_URL}/api/api_bisogeo.php`,
-        {
-          params: { inp: text },
-        }
-      );
-      return response.data.resultCode === "Y";
-    } catch (error) {
-      logger.error("Profanity Check Error:", error);
-      return true;
-    }
+  checkProfanity: (text: string) => {
+    logger.info(`Profanity Check Text: ${text}`);
+    return axios.get<ProfanityResponse>(`${BASE_URL}/api/api_bisogeo.php`, {
+      params: { inp: text },
+    });
   },
 };
